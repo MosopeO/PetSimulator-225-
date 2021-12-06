8c9e5b32a7f89afdb32c24b3cf96e96d5f54789e
Eliyana Brown <eliyanapearl@gmail.com>
2021-12-06 16:33:10 -0500

    animations and more

--- js/cat.js
+++ js/cat.js
@@ -7,6 +7,8 @@ var sleep = 0;
 var speak = 0;
 
 $('#msgBox').hide();
+$('#collar').hide();
+$('#food').hide();
 
 $('#feed').click(function(){
     lovePoints += 2;
@@ -17,56 +19,14 @@ $('#feed').click(function(){
         $('#message').text("Your pet needs to play");
         setTimeout(function() { $("#msgBox").hide(); }, 3000);
     }
-    if(lovePoints == 500){
-        $('#msgBox').show();
-        $('#message').text("Congrats, you maxed out the love bar! You and your pet are best friends! Keep taking care of your pet to earn even more points.");
-        setTimeout(function() { $("#msgBox").hide(); }, 10000);
-    }
-    else if(lovePoints%500 == 0 && lovePoints != 0){
-        $('#msgBox').show();
-        $('#message').text("Congrats, you've earned " + lovePoints + " points! Keep up the good work.");
-        setTimeout(function() { $("#msgBox").hide(); }, 5000);
-    }
-    if(lovePoints%100 == 0 && lovePoints <= 500){
-        if(lovePoints == 100){
-            document.getElementById('heart').animate([{transform: 'translateX(0px)'},{transform: 'translateX(50px)'}],{duration: 2500, fill: "forwards"});}
-        else if(lovePoints == 200){
-            document.getElementById('heart').animate([{transform: 'translateX(50px)'},{transform: 'translateX(100px)'}],{duration: 2500, fill: "forwards"});}
-        else if(lovePoints == 300){
-            document.getElementById('heart').animate([{transform: 'translateX(100px)'},{transform: 'translateX(150px)'}],{duration: 2500, fill: "forwards"});}
-        else if(lovePoints == 400){
-            document.getElementById('heart').animate([{transform: 'translateX(150px)'},{transform: 'translateX(200px)'}],{duration: 2500, fill: "forwards"});}
-        else if(lovePoints == 500){
-            document.getElementById('heart').animate([{transform: 'translateX(200px)'},{transform: 'translateX(250px)'}],{duration: 2500, fill: "forwards"});}
-    }
 });
+
 $('#pet').click(function(){
     lovePoints += 1;
     pet += 1;
     console.log(lovePoints + ", " + pet);
-    if(lovePoints == 500){
-        $('#msgBox').show();
-        $('#message').text("Congrats, you maxed out the love bar! You and your pet are best friends! Keep taking care of your pet to earn even more points.");
-        setTimeout(function() { $("#msgBox").hide(); }, 10000);
-    }
-    else if(lovePoints%500 == 0 && lovePoints != 0){
-        $('#msgBox').show();
-        $('#message').text("Congrats, you've earned " + lovePoints + " points! Keep up the good work.");
-        setTimeout(function() { $("#msgBox").hide(); }, 5000);
-    }
-    if(lovePoints%100 == 0 && lovePoints <= 500){
-        if(lovePoints == 100){
-            document.getElementById('heart').animate([{transform: 'translateX(0px)'},{transform: 'translateX(50px)'}],{duration: 2500, fill: "forwards"});}
-        else if(lovePoints == 200){
-            document.getElementById('heart').animate([{transform: 'translateX(50px)'},{transform: 'translateX(100px)'}],{duration: 2500, fill: "forwards"});}
-        else if(lovePoints == 300){
-            document.getElementById('heart').animate([{transform: 'translateX(100px)'},{transform: 'translateX(150px)'}],{duration: 2500, fill: "forwards"});}
-        else if(lovePoints == 400){
-            document.getElementById('heart').animate([{transform: 'translateX(150px)'},{transform: 'translateX(200px)'}],{duration: 2500, fill: "forwards"});}
-        else if(lovePoints == 500){
-            document.getElementById('heart').animate([{transform: 'translateX(200px)'},{transform: 'translateX(250px)'}],{duration: 2500, fill: "forwards"});}
-    }
 });
+
 $('#play').click(function(){
     lovePoints += 1;
     play += 1;
@@ -76,56 +36,14 @@ $('#play').click(function(){
         $('#message').text("Your pet needs some sleep");
         setTimeout(function() { $("#msgBox").hide(); }, 3000);
     }
-    if(lovePoints == 500){
-        $('#msgBox').show();
-        $('#message').text("Congrats, you maxed out the love bar! You and your pet are best friends! Keep taking care of your pet to earn even more points.");
-        setTimeout(function() { $("#msgBox").hide(); }, 10000);
-    }
-    else if(lovePoints%500 == 0 && lovePoints != 0){
-        $('#msgBox').show();
-        $('#message').text("Congrats, you've earned " + lovePoints + " points! Keep up the good work.");
-        setTimeout(function() { $("#msgBox").hide(); }, 5000);
-    }
-    if(lovePoints%100 == 0 && lovePoints <= 500){
-        if(lovePoints == 100){
-            document.getElementById('heart').animate([{transform: 'translateX(0px)'},{transform: 'translateX(50px)'}],{duration: 2500, fill: "forwards"});}
-        else if(lovePoints == 200){
-            document.getElementById('heart').animate([{transform: 'translateX(50px)'},{transform: 'translateX(100px)'}],{duration: 2500, fill: "forwards"});}
-        else if(lovePoints == 300){
-            document.getElementById('heart').animate([{transform: 'translateX(100px)'},{transform: 'translateX(150px)'}],{duration: 2500, fill: "forwards"});}
-        else if(lovePoints == 400){
-            document.getElementById('heart').animate([{transform: 'translateX(150px)'},{transform: 'translateX(200px)'}],{duration: 2500, fill: "forwards"});}
-        else if(lovePoints == 500){
-            document.getElementById('heart').animate([{transform: 'translateX(200px)'},{transform: 'translateX(250px)'}],{duration: 2500, fill: "forwards"});}
-    }
 });
+
 $('#clean').click(function(){
     lovePoints -= 1;
     clean += 1;
     console.log(lovePoints + ", " + clean);
-    if(lovePoints == 500){
-        $('#msgBox').show();
-        $('#message').text("Congrats, you maxed out the love bar! You and your pet are best friends! Keep taking care of your pet to earn even more points.");
-        setTimeout(function() { $("#msgBox").hide(); }, 10000);
-    }
-    else if(lovePoints%500 == 0 && lovePoints != 0){
-        $('#msgBox').show();
-        $('#message').text("Congrats, you've earned " + lovePoints + " points! Keep up the good work.");
-        setTimeout(function() { $("#msgBox").hide(); }, 5000);
-    }
-    if(lovePoints%100 == 0 && lovePoints <= 500){
-        if(lovePoints == 100){
-            document.getElementById('heart').animate([{transform: 'translateX(0px)'},{transform: 'translateX(50px)'}],{duration: 2500, fill: "forwards"});}
-        else if(lovePoints == 200){
-            document.getElementById('heart').animate([{transform: 'translateX(50px)'},{transform: 'translateX(100px)'}],{duration: 2500, fill: "forwards"});}
-        else if(lovePoints == 300){
-            document.getElementById('heart').animate([{transform: 'translateX(100px)'},{transform: 'translateX(150px)'}],{duration: 2500, fill: "forwards"});}
-        else if(lovePoints == 400){
-            document.getElementById('heart').animate([{transform: 'translateX(150px)'},{transform: 'translateX(200px)'}],{duration: 2500, fill: "forwards"});}
-        else if(lovePoints == 500){
-            document.getElementById('heart').animate([{transform: 'translateX(200px)'},{transform: 'translateX(250px)'}],{duration: 2500, fill: "forwards"});}
-    }
 });
+
 $('#sleep').click(function(){
     lovePoints += 2;
     sleep +=1;
@@ -135,67 +53,79 @@ $('#sleep').click(function(){
         $('#message').text("Your pet needs to play");
         setTimeout(function() { $("#msgBox").hide(); }, 3000);
     }
-    if(lovePoints == 500){
-        $('#msgBox').show();
-        $('#message').text("Congrats, you maxed out the love bar! You and your pet are best friends! Keep taking care of your pet to earn even more points.");
-        setTimeout(function() { $("#msgBox").hide(); }, 10000);
-    }
-    else if(lovePoints%500 == 0 && lovePoints != 0){
-        $('#msgBox').show();
-        $('#message').text("Congrats, you've earned " + lovePoints + " points! Keep up the good work.");
-        setTimeout(function() { $("#msgBox").hide(); }, 5000);
-    }
-    if(lovePoints%100 == 0 && lovePoints <= 500){
-        if(lovePoints == 100){
-            document.getElementById('heart').animate([{transform: 'translateX(0px)'},{transform: 'translateX(50px)'}],{duration: 2500, fill: "forwards"});}
-        else if(lovePoints == 200){
-            document.getElementById('heart').animate([{transform: 'translateX(50px)'},{transform: 'translateX(100px)'}],{duration: 2500, fill: "forwards"});}
-        else if(lovePoints == 300){
-            document.getElementById('heart').animate([{transform: 'translateX(100px)'},{transform: 'translateX(150px)'}],{duration: 2500, fill: "forwards"});}
-        else if(lovePoints == 400){
-            document.getElementById('heart').animate([{transform: 'translateX(150px)'},{transform: 'translateX(200px)'}],{duration: 2500, fill: "forwards"});}
-        else if(lovePoints == 500){
-            document.getElementById('heart').animate([{transform: 'translateX(200px)'},{transform: 'translateX(250px)'}],{duration: 2500, fill: "forwards"});}
-    }
 });
 $('#speak').click(function(){
     lovePoints += 1;
     speak += 1;
     console.log(lovePoints + ", " + speak);
-    if(lovePoints == 500){
+});
+
+$('#submit').click(function(){
+    var name = $('#pname').val();
+    $('#displayName').text(name);
+    console.log(name); 
+});
+
+$(':button').click(function(){
+    var name = $('#pname').val();
+    $('#points').text(lovePoints + " pts");
+    if(lovePoints == 50){
         $('#msgBox').show();
-        $('#message').text("Congrats, you maxed out the love bar! You and your pet are best friends! Keep taking care of your pet to earn even more points.");
+        $('#message').text("Congrats, you maxed out the love bar! You and "+ name +" are best friends! Keep taking care of your pet to earn even more points.");
         setTimeout(function() { $("#msgBox").hide(); }, 10000);
     }
-    else if(lovePoints%500 == 0 && lovePoints != 0){
+    else if(lovePoints%50 == 0 && lovePoints != 0){
         $('#msgBox').show();
         $('#message').text("Congrats, you've earned " + lovePoints + " points! Keep up the good work.");
         setTimeout(function() { $("#msgBox").hide(); }, 5000);
     }
-    if(lovePoints%100 == 0 && lovePoints <= 500){
-        if(lovePoints == 100){
+    if(lovePoints%10 == 0 && lovePoints <= 50){
+        if(lovePoints == 10){
             document.getElementById('heart').animate([{transform: 'translateX(0px)'},{transform: 'translateX(50px)'}],{duration: 2500, fill: "forwards"});}
-        else if(lovePoints == 200){
+        else if(lovePoints == 20){
             document.getElementById('heart').animate([{transform: 'translateX(50px)'},{transform: 'translateX(100px)'}],{duration: 2500, fill: "forwards"});}
-        else if(lovePoints == 300){
+        else if(lovePoints == 30){
             document.getElementById('heart').animate([{transform: 'translateX(100px)'},{transform: 'translateX(150px)'}],{duration: 2500, fill: "forwards"});}
-        else if(lovePoints == 400){
+        else if(lovePoints == 40){
             document.getElementById('heart').animate([{transform: 'translateX(150px)'},{transform: 'translateX(200px)'}],{duration: 2500, fill: "forwards"});}
-        else if(lovePoints == 500){
+        else if(lovePoints == 50){
             document.getElementById('heart').animate([{transform: 'translateX(200px)'},{transform: 'translateX(250px)'}],{duration: 2500, fill: "forwards"});}
     }
+    if(lovePoints > 100){
+        $('#collar').show();
+    }
 });
+
+$('#feed').click(function(){
+    if(lovePoints >= 50){
+        $('#food').attr('src', 'img/cat_golden_treat.png');
+    }
+    $('#food').show();
+    $('#food').animate({right: '+=510px', top: '+=370px'}, 2000);
+    $('#food').fadeOut(3000, function(){
+        $('#food').animate({right: '30%', top: '15%'});
+    });
+});
+
 $('#pet').on('click', function () {
-    show(document.getElementById("petHand"), 5000);
+    $('#petHand').show();
+    document.getElementById('petHand').animate([{transform: 'translateX(0px)'},{transform: 'translateX(-25px)'}],{duration: 500});
+    $('#petHand').fadeOut(3000);
 });
 
 $('#play').on('click', function () {
+    if(lovePoints >= 150){
+        $('#yarn').attr('src', 'img/cat_golden_toy.png');
+    }
     show(document.getElementById("yarn"), 5000);
+    $('#yarn').animate({left:'-=200px', rotate: '-720deg'},5000);
+    $('#yarn').animate({top: '90%', left: '45%'});
 });
 
 $('#clean').on('click', function () {
     console.log("in the function");
     show(document.getElementById("shower"), 5000);
+    document.getElementById("shower").animate([{transform: 'translateX(0px'},{transform: 'translateX(-55px)'}],{duration:5000});
 });
 
 $('#sleep').on('click', function () {
